69f7f9e5d563c1ee827a3e0bebb6cc1fa0a44315
Marc Choisy <mchoisy@gmail.com>
2015-07-10 16:37:10 +0700

    Initial commit: created the three data sets of chapter 1.

---
+++ R/datasets.R
@@ -0,0 +1,50 @@
+#' AIDS reported cases in the USA (1982-1990)
+#'
+#' A data set containing the number of reported AIDS case per quarter
+#' in the USA from 1982 to 1990.
+#'
+#' @format A time-series data structure.
+#' @source
+#' Hay, J.W. and Wolak, F.A. (1994) A procedure for estimating the unconditional
+#' cumulative incidence curve and its variability for the human immunodeficiency
+#' virus. Journal of the Royal Statistical Society C43, 599-624.
+#'
+#' Rosenberg, P.S. (1990) A simple correction of AIDS surveillance data for
+#' reporting delays. J. Acq. Immune Def. Synd. 3, 49-54.
+#' @examples
+#' data(aids_usa)
+#' plot(aids_usa,type="o",pch=19,lty=2,xlab="time",ylab="incidence")
+"aids_usa"
+
+#' AIDS reported cases in the UK (1982-1986)
+#'
+#' A data set containing the number of reported AIDS case per month
+#' in the UK from 1982 to 1990.
+#'
+#' @format A time-series data structure.
+#' @source
+#' Healy M., Tillett H. (1988) Short-term extrapolation of the AIDS epidemic.
+#' J. Roy. Stat. Soc. A 151, 50-61.
+#' @examples
+#' data(aids_uk)
+#' plot(aids_uk,type="o",pch=19,lty=2,xlab="time",ylab="incidence")
+"aids_uk"
+
+#' AIDS reported cases in Australia (1982-1988)
+#'
+#' A data set containing the number of reported AIDS case per month
+#' in the UK from 1982 to 1990.
+#'
+#' @format A time-series data structure.
+#' @source
+#' Daley D, Gani J (1999) Epidemic modelling: an introduction. Cambridge University
+#' Press, Cambridge, MA.
+#'
+#' Wilson S.R. (1989) Extrapolation forecasting. In Predicting the Course of AIDS
+#' in Australia and Evaluating the Effect of AZT: A First Report, by Solomon P.J.,
+#' Doust J.A. and Wilson S.R., National Centre for Epidemiology and Public Health,
+#' Working Paper 3, Australian National University, Canberra, 33-41.
+#' @examples
+#' data(aids_australia)
+#' plot(aids_australia,type="o",pch=19,lty=2,xlab="time",ylab="incidence")
+"aids_australia"
---
+++ R/midp.R
@@ -0,0 +1,14 @@
+#' midp: Modeling infectious diseases parameters based on serological and social
+#' contact data.
+#'
+#' The midp package is based on the book "Modeling infectious diseases
+#' parameters based on serological and social contact data" by Niel Hens, Ziv
+#' Shkedy, Marc Aerts, Christel Faes, Pierre Van Damme and Philippe Beutels (
+#' Springer, 2012).
+#'
+#' @section midp functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name midp
+NULL
